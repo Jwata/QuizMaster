4b4094fe280a322e64132269ebb85e6ad3b502a9
Jwata <watanabe0621@gmail.com>
2017-12-06 23:07:43 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Demo data
+## User
+User.find_or_create_by!(id: 1)
+
+## Questions
+Question.find_or_create_by!(id: 1) do |question|
+  question.content = "# Which is the highest mountain?\r\n\r\n|Mount Kilimanjaro|Mount Fuji|Denali|\r\n|---|---|---|\r\n|![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Mount_Kilimanjaro.jpg/200px-Mount_Kilimanjaro.jpg)|![](https://upload.wikimedia.org/wikipedia/ja/thumb/3/3e/MtFuji_FujiCity.jpg/200px-MtFuji_FujiCity.jpg)|![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Wonder_Lake_and_Denali.jpg/200px-Wonder_Lake_and_Denali.jpg)|\r\n"
+  question.answer = 'Denali'
+end
+
+Question.find_or_create_by!(id: 2) do |question|
+  question.content = "# What number should come next in the pattern?\r\n\r\n1, 1, 2, 3, 5, 8, 13"
+  question.answer = '21'
+end
+
+Question.find_or_create_by!(id: 3) do |question|
+  question.content = "# How high is Mount Fuji in meters?\r\n( *answer with meters like \"1234 meters\"* )\r\n\r\n![](https://upload.wikimedia.org/wikipedia/ja/thumb/3/3e/MtFuji_FujiCity.jpg/400px-MtFuji_FujiCity.jpg\" alt=\"MtFuji FujiCity.jpg)"
+  question.answer = '3776 meters'
+end
