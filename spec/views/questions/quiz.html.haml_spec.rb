6e4943e8db4ba5c947b9e9e0055825b3520bc5a8
Jwata <watanabe0621@gmail.com>
2017-12-12 02:06:49 +0900

    display next quiz button on the quiz answer page

--- spec/views/questions/quiz.html.haml_spec.rb
+++ spec/views/questions/quiz.html.haml_spec.rb
@@ -15,5 +15,33 @@ RSpec.describe 'questions/quiz', type: :view do
       assert_select 'input[name=?]', 'quiz[answer]'
     end
   end
-end
 
+  context 'when completed' do
+    helper_method :current_learning_session
+
+    before do
+      flash[:quiz_answer] = true
+      allow(view).to receive(:current_learning_session).and_return(learning_session)
+    end
+
+    context 'when no learning session' do
+      let(:learning_session) { nil }
+
+      it 'shows next actions' do
+        render template: 'questions/quiz'
+        expect(rendered).to match(/Try Again/)
+        expect(rendered).to match(/Show Answer/)
+        expect(rendered).to match(/Back to List/)
+      end
+    end
+
+    context 'during a learning session' do
+      let(:learning_session) { double(:learning_session) }
+
+      it 'shows next quiz button' do
+        render template: 'questions/quiz'
+        expect(rendered).to match(/Next Quiz/)
+      end
+    end
+  end
+end
